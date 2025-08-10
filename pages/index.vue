cac1483e5c71f277db835d9632182b51445ce852
Sylvere <sylvere.armange@proton.me>
2025-08-10 12:09:50 +0200

    Testing preview

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div
     class="w-full h-screen flex flex-col items-center justify-center bg-white text-black">
     <p class="font-cabin">{{ $t("test") }}</p>
+    <Countdown></Countdown>
   </div>
 </template>
 
