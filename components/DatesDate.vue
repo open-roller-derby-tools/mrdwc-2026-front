c117bbf4f3bdd64d84967f8f1b9ede06ce4f3653
Sylvere <sylvere.armange@proton.me>
2025-08-11 01:15:46 +0200

    Added text paragraphs with temporary French translations

--- components/DatesDate.vue
+++ components/DatesDate.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-col">
-    <p class="text-[1.2em]">{{ $d(date, "d") }}</p>
+    <p class="text-[1.2em]">{{ d(date, "d") }}</p>
     <p
       class="text-[1em] uppercase"
       :class="{ 'order-first': month_first, '-mb-[0.5em]': month_first, '-mt-[0.5em]': !month_first }"
-    >{{ $d(date, "m") }}</p>
+    >{{ d(date, "m") }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { t } = useI18n();
+const { t, d } = useI18n();
 
 const props = defineProps<{
   date: Date
--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,33 @@
     </section>
     <section class="bg-blue-dark py-8">
       <div class="maxed flex flex-col sm:flex-row gap-16">
-        <div>
+        <div class="w-full sm:w-2/5">
           <Venues></Venues>
           <OfficialLogos></OfficialLogos>
         </div>
-        <div class="">
+        <div class="w-full sm:w-3/5 text-lg">
           <ParagraphTitle :title="t('teams_application')"></ParagraphTitle>
+          <div class="text-pretty mb-12">
+            <p>{{ t('teams_application_text.line1') }}</p>
+            <h3 class="text-yellow font-bold uppercase text-xl mt-6 mb-2">{{ t('timelines') }}</h3>
+            <p
+              class="mb-2"
+              v-html="t('teams_application_text.date1', { date: getFormattedDate(teamsDate1) })"
+            ></p>
+            <p v-html="t('teams_application_text.date2', { date: getFormattedDate(teamsDate2) })"></p>
+          </div>
           <ParagraphTitle :title="t('officials_application')"></ParagraphTitle>
+          <div class="text-pretty mb-12">
+            <p class="mb-6">{{ t('officials_application_text.line1') }}</p>
+            <p class="">{{ t('officials_application_text.line2') }}</p>
+            <p v-html="t('officials_application_text.date1', { date: getFormattedDate(officialsDate1) })"></p>
+            <h3 class="text-yellow font-bold uppercase text-xl mt-6 mb-2">{{ t('timelines') }}</h3>
+            <p
+              class="mb-2"
+              v-html="t('officials_application_text.date2', { date: getFormattedDate(officialsDate2) })"
+            ></p>
+            <p v-html="t('officials_application_text.date3', { date: getFormattedDate(officialsDate3) })"></p>
+          </div>
         </div>
       </div>
     </section>
@@ -35,12 +55,24 @@
 
 <script setup>
 const img = useImage();
-const { t } = useI18n();
+const { t, d } = useI18n();
 
 const backgroundStyles = computed(() => {
   const imgUrl = img('background@2x.png');
   return { backgroundImage: `url('${imgUrl}')` }
 })
+
+const teamsDate1 = new Date(2025, 8, 1, 0, 0, 0);
+const teamsDate2 = new Date(2025, 9, 1, 0, 0, 0);
+const officialsDate1 = new Date(2025, 7, 25, 0, 0, 0);
+const officialsDate2 = new Date(2025, 8, 12, 0, 0, 0);
+const officialsDate3 = new Date(2025, 9, 31, 0, 0, 0);
+
+const getFormattedDate = (date) => {
+  return `<strong>${d(date, "dmy")}</strong>`;
+  // return d(officialsDate1, "dmy");
+};
+
 </script>
 
 <style scoped></style>
