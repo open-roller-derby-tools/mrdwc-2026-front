fdb7e4c29194e6714f8652ddb0f1f0374841228f
Sylvere <sylvere.armange@proton.me>
2025-10-07 15:53:50 +0200

    Auto-select tab from url hash.

--- app/components/blocks/BlockTabs.vue
+++ app/components/blocks/BlockTabs.vue
@@ -6,14 +6,15 @@
     <ul class="maxed padded mt-6 flex flex-col sm:flex-row gap-0.5 items-center">
       <li
         v-for="slug, i in data.tabs"
-        @click="activeTabIndex = i"
+        @click="activateTab(slug)"
         class="transition-colors duration-100 font-shoulders font-semibold text-xl px-3 py-2 w-full sm:w-auto flex gap-4 items-center select-none cursor-pointer first:rounded-t-xl last:rounded-b-xl sm:first:rounded-tr-none sm:first:rounded-bl-xl sm:last:rounded-bl-none sm:last:rounded-tr-xl"
-        :class="activeTabIndex == i ? 'bg-yellow text-blue-text' : 'bg-blue-light text-white'"
+        :class="activeSlug == slug ? 'bg-yellow text-blue-text' : 'bg-blue-light text-white'"
+        :data-slug="slug"
       >{{ getPageWithSlug(slug)?.menu_title || slug }}
         <UIcon
           name="i-lucide-arrow-right"
           class="size-7 transition-transform duration-200 ease-out"
-          :class="activeTabIndex == i ? 'rotate-90' : ''"
+          :class="activeSlug == slug ? 'rotate-90' : ''"
         />
       </li>
     </ul>
@@ -32,19 +33,28 @@ const props = defineProps<{
   data: ILocalizedBlockTabs
 }>()
 
-const activeTabIndex = ref(0)
-const activeTab = computed(() => {
-  return props.data.tabs[activeTabIndex.value] || null
+const route = useRoute()
+const activeSlug = ref<string | undefined>(undefined)
+
+onMounted(() => {
+  activeSlug.value = props.data.tabs.find((slug) => slug === route.hash.slice(1))
+  if (!activeSlug.value && props.data.tabs.length > 0) {
+    activeSlug.value = props.data.tabs[0]
+  }
 })
 
 const pagesStore = usePagesStore()
 const { getPageWithSlug } = storeToRefs(pagesStore)
 const activePage = computed((): ILocalizedPage | null => {
-  if (activeTab.value)
-    return getPageWithSlug.value(activeTab.value)
+  if (activeSlug.value)
+    return getPageWithSlug.value(activeSlug.value)
   return null
 })
 
+const activateTab = (slug: string) => {
+  activeSlug.value = slug
+  navigateTo(`${route.path}#${slug}`)
+}
 </script>
 
 <style></style>
