76f14df1c6b596ac7d760561f7e112ecb1275b3f
Sylvere <sylvere.armange@proton.me>
2025-10-07 16:35:53 +0200

    Fixed map URL link.

--- app/components/partials/Venue.vue
+++ app/components/partials/Venue.vue
@@ -2,12 +2,9 @@
   <div class="my-4 text-balance">
     <p class="font-bold text-2xl mb-2 leading-snug">
       {{ venue.name }}
-      <span
-        class="uppercase mx-2 text-xs border-1 border-red-light text-red-light block-inline px-2 py-0.5 rounded-full bg-red-light/20"
-        >{{
-          venue.type === "main" ? t("venue_primary") : t("venue_secondary")
-        }}</span
-      >
+      <span class="uppercase mx-2 text-xs border-1 border-red-light text-red-light block-inline px-2 py-0.5 rounded-full bg-red-light/20">{{
+        venue.type === "main" ? t("venue_primary") : t("venue_secondary")
+      }}</span>
     </p>
     <address class="font-medium text-lg leading-snug -mt-[0.2em]">
       {{ venue.address }}
@@ -21,11 +18,10 @@
         leadingIcon:
           'text-blue-light size-[32px] transition-transform duration-200 group-hover:-rotate-90',
       }"
-      to="venue.map_url"
+      :to="venue.map_url"
       target="_blank"
       rel="noopener noreferrer"
-      >{{ t("see_map") }}</UButton
-    >
+    >{{ t("see_map") }}</UButton>
   </div>
 </template>
 
