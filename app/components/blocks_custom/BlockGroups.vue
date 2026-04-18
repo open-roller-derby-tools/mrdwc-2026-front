c71b1045b8f084409f32f212c09ad60e398acc0a
Sylvere <sylvere.armange@proton.me>
2026-04-18 14:56:50 +0200

    Better styling for groups block

--- app/components/blocks_custom/BlockGroups.vue
+++ app/components/blocks_custom/BlockGroups.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="w-full maxed padded my-8">
-    <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <div
-        v-for="group in groups"
-        :key="group.id"
-        class="bg-white text-black rounded-2xl p-4"
-      >
-        <h3 class="font-bold text-2xl text-red-text">
-          {{ t("group", { number: group.number }) }}
-        </h3>
-        <ul class="list-none text-base">
-          <li v-for="team in group.teams" :key="team">
-            <NuxtLink :to="`/teams/${getTeamById(team)?.slug}`">
-              <span
-                class="font-mono font-bold px-1.5 mr-2 bg-yellow text-black rounded-md"
-              >
-                {{ getTeamById(team)?.name_letters }}</span
-              >
-              <span class="font-medium">
-                {{ getTeamById(team)?.name }}
-              </span>
-            </NuxtLink>
-          </li>
-        </ul>
+  <div class="w-full bg-blue-text">
+    <div class="w-full maxed padded py-8">
+      <h2 class="relative sm:-left-2.5 flex items-center mb-2">
+        <u-icon
+          name="i-lucide-arrow-down-right"
+          class="text-yellow size-8 sm:size-12"
+        />{{ t("groups") }}
+      </h2>
+      <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div
+          v-for="group in groups"
+          :key="group.id"
+          class="bg-white text-black rounded-2xl p-4"
+        >
+          <h3 class="font-bold text-2xl text-red-text">
+            {{ t("group", { number: group.number }) }}
+          </h3>
+          <ul class="list-none text-base">
+            <li v-for="team in group.teams" :key="team">
+              <NuxtLink :to="`/teams/${getTeamById(team)?.slug}`">
+                <span
+                  class="font-mono font-bold px-1.5 mr-2 bg-yellow text-black rounded-md"
+                >
+                  {{ getTeamById(team)?.name_letters }}</span
+                >
+                <span class="font-medium">
+                  {{ getTeamById(team)?.name }}
+                </span>
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
